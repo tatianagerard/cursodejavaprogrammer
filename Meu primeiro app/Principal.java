06f2f1c31685f554bd4de9d28c33c0c4e9fa7a79
Tatiana Meneghelli <103676114+tatianagerard@users.noreply.github.com>
2023-03-29 19:40:04 -0300

    Exesrcicio dia 28

---
+++ Meu primeiro app/Principal.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Principal{
+
+	public static void main(String[] args){
+		
+		
+		System.out.println("############Cadastro de Aluno###############");
+
+		System.out.println("Digite o seu nome:");
+
+		Scanner entradaDados = new Scanner(System.in);
+
+		String nome = entradaDados.next();
+
+		System.out.println("O nome digitado foi: ");
+		System.out.println(nome);
+
+                System.out.println("O nome digitado foi: ");
+		System.out.println(idade);
+
+             System.out.println("O nome digitado foi: ");
+		System.out.println(endereço);
